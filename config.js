482199c7726d1d26162e82c87fb6870edf07ac3f
Jaime Jimenez <jaimejimenez99@hotmail.com>
2022-12-22 11:59:34 +0100

    Changes

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     mysqlConfig: {
-        host: "localhost",
-        user: "root",
-        password: "",
+        host: "tis.cvmsi2cvvkqw.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "TISBFJ123",
         database: "TIS"
     },
     port: 3000
